a590d74c37d8a4c7736a93817b89f0bd2be52467
Trystan Spalding-Jenkin <Trystan.Spalding-Jenkin@doherty.co.uk>
2022-03-24 15:22:02 +0000

    remove PS-NCentral

--- GetFromAzureTenant/run.ps1
+++ GetFromAzureTenant/run.ps1
@@ -15,12 +15,12 @@ $AzureTenantGUID = $Request.Query.ID
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 #Register-PSRepository -Default
 
-if ( -Not (Get-Module PS-NCentral)) {
-    Install-Module -Name PS-NCentral
-}
+#if ( -Not (Get-Module PS-NCentral)) {
+#    Install-Module -Name PS-NCentral
+#}
 
 #Connect to NC
-New-NCentralConnection -ServerFQDN $serverHost -JWT $JWT | Out-Null
+#New-NCentralConnection -ServerFQDN $serverHost -JWT $JWT | Out-Null
 
 $CustRestBody = 
 @"
@@ -60,11 +60,36 @@ $Customers = ForEach ($Entity in $CustomerList) {
 
 #$Customers | Out-GridView
 
+$PropsRestBody = 
+@"
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ei2="http://ei2.nobj.nable.com/">
+   <soap:Header/>
+   <soap:Body>
+      <ei2:organizationPropertyList>
+          <ei2:username>$null</ei2:username>
+          <ei2:password>$JWT</ei2:password>
+         <ei2:reverseOrder>false</ei2:reverseOrder>
+      </ei2:organizationPropertyList>
+   </soap:Body>
+</soap:Envelope>
+"@
+
+Try {
+    $PropertiesReturn = (Invoke-RestMethod -Uri $bindingURL -body $PropsRestBody -Method POST).Envelope.body.organizationPropertyListResponse.return
+    $PropertiesList = ProcessData1 $PropertiesReturn "properties"
+}
+Catch {
+    Write-Host "Could not connect: $($_.Exception.Message)"
+    exit
+}
+
+#$PropertiesList | Out-GridView
+#return
 
 #Merge
 foreach ($Customer in $Customers) {
     #Write-Host "querying: $($Customer.id)"
-    $Properties = Get-NCCustomerPropertyList $Customer.id
+    $Properties = $PropertiesList | Where-Object { $_.customerid -eq $Customer.id }
     $AzureTenantProperty = $Properties.psobject.properties['AzureTenantGUID']
     #Write-Host "AzureTenantGUID: $($AzureTenantProperty)"
 
@@ -85,3 +110,80 @@ Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::NotFound
     Body       = $Null
 })
+
+function ProcessData1([Array]$InArray,[String]$PairClass){
+		
+    ## Received Dataset KeyPairs 2 List/Columns
+    $OutObjects = @()
+    
+    if ($InArray){
+        foreach ($InObject in $InArray) {
+
+#				$ThisObject = New-Object PSObject				## In this routine the object is created at start. Properties are added with values.
+            $Props = @{}									## In this routine the object is created at the end. Properties from a list.
+
+            ## Add a Reference-Column at Object-Level
+            If ($PairClass -eq "Properties"){
+                ## CustomerLink if Available
+                if(Get-Member -inputobject $InObject -name "CustomerID"){
+#						$ThisObject | Add-Member -MemberType NoteProperty -Name 'CustomerID' -Value $InObject.CustomerID -Force
+                    $Props.add('CustomerID',$InObject.CustomerID)
+                }
+                
+                ## DeviceLink if Available
+                if(Get-Member -inputobject $InObject -name "DeviceID"){
+#						$ThisObject | Add-Member -MemberType NoteProperty -Name 'DeviceID' -Value $InObject.DeviceID -Force
+                    $Props.add('DeviceID',$InObject.DeviceID)
+                }
+            }
+
+            ## Convert all (remaining) keypairs to Properties
+            foreach ($item in $InObject.$PairClass) {
+
+                ## Cleanup the Key and/or Value before usage.
+                If ($PairClass -eq "Properties"){
+                    $Header = $item.label
+                }
+                Else{
+                    If($item.key.split(".")[0] -eq 'asset'){	##Should use ProcessData2 (ToDo)
+                        $Header = $item.key
+                    }
+                    Else{
+                        $Header = $item.key.split(".")[1]
+                    }
+                }
+
+                ## Ensure a Flat Value
+                If ($item.value -is [Array]){
+                    $DataValue = $item.Value[0]
+                }
+                Else{
+                    $DataValue = $item.Value
+                }
+
+                ## Now add the Key/Value pairs.
+#					$ThisObject | Add-Member -MemberType NoteProperty -Name $Header -Value $DataValue -Force
+
+                 # if a key is found that already exists in the hashtable
+                if ($Props.ContainsKey($Header)) {
+                    # either overwrite the value 'Last-One-Wins'
+                    # or do nothing 'First-One-Wins'
+                    #if ($this.allowOverwrite) { $Props[$Header] = $DataValue }
+                }
+                else {
+                    $Props[$Header] = $DataValue
+                }					
+#					$Props.add($Header,$DataValue)
+
+            }
+            $ThisObject = New-Object -TypeName PSObject -Property $Props	#Alternative option
+
+            ## Add the Object to the list
+            $OutObjects += $ThisObject
+        }
+    }
+    ## Return the list of Objects
+    Return $OutObjects
+#		$OutObjects
+#		Write-Output $OutObjects
+}
